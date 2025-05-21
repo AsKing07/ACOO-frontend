69611404bdef8b69c9cc75941628534b9618419f
Charbel SONON <charbelsnn@gmail.com>
2025-05-21 15:53:58 +0200

    add footer component

---
+++ templates/components/footer.php
@@ -0,0 +1,44 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles.css">
+</head>
+<body>
+
+<footer class="footer">
+  <div class="footer__container">
+    <div class="footer__block footer__block--club">
+      <div class="footer__block-title">ACOO - Club d’aviron</div>
+      <div class="footer__block-desc">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+      </div>
+    </div>
+    <div class="footer__divider"></div>
+    <div class="footer__block footer__block--links">
+      <div class="footer__block-title">LIENS</div>
+      <nav class="footer__links">
+        <a href="#">ACCUEIL</a>
+        <a href="#">CLUB</a>
+        <a href="#">ÉVÉNEMENTS</a>
+        <a href="#">GALERIE</a>
+        <a href="#">PALMARÈS</a>
+        <a href="#">CONTACT</a>
+      </nav>
+    </div>
+    <div class="footer__divider"></div>
+    <div class="footer__block footer__block--contact">
+      <div class="footer__block-title">CONTACT</div>
+      <div class="footer__contact-list">
+        <div>Téléphone : 02 35 39 23 45</div>
+        <div>Email : acoo@acoo.fr</div>
+      </div>
+    </div>
+    <div class="footer__copyright">
+      ACOO x EEMI - Copyright @2025
+    </div>
+  </div>
+</footer>
+    
+</body>
+</html>
--- templates/index.php
+++ templates/index.php
@@ -1,6 +1,8 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TEMPLATE</title>
     <style>
@@ -25,6 +27,8 @@
 <?php require_once 'components/buttons.php'; ?>
 
 <?php require_once 'components/events_card.php'; ?>
+
+<?php require_once 'components/footer.php'; ?>
 
 
     
