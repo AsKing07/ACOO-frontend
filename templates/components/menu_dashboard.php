10a1bbe7048a3d19867c11f65c976bc092e56323
alvyn <alvyn.richard@hotmail.fr>
2025-05-22 13:43:56 +0200

    menu navigation of dashboard do

---
+++ templates/components/menu_dashboard.php
@@ -0,0 +1,87 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../styles.css">
+</head>
+<body>
+    
+    <div class="container_navigation">
+
+        <nav class="container_nav_first">
+
+            <div class="container_nav_logo">
+                <img src="../../images/Logo.png" alt="Logo Acoo">
+            </div>
+
+            <div class="container_nav_second">
+
+                <div class="container_nav_pages">
+                    <div>
+                        <div>
+                            <h5>Pages</h5>
+                        </div>
+                        <ul>
+                            <li>
+                                <img src="../../../image/accueil.png" alt="acceuil">
+                                <span>ACCEUIL</span>
+                            </li>
+                            <li>
+                                <img src="../../../image/aviron-silhouette.png" alt="club">
+                                <span>CLUB</span>
+                            </li>
+                            <li>
+                                <img src="../../../image/etoile.png" alt="palmares">
+                                <span>PALMARES</span>
+                            </li>
+                            <li>
+                                <img src="../../../image/partenaire.png" alt="partenaire">
+                                <span>PARTENAIRES</span>
+                            </li>
+                            <li>
+                                <img src="../../../image/soleil.png" alt="actus">
+                                <span>ACTUS</span>
+                            </li>
+                            <li>
+                                <img src="../../../image/calendrier.png" alt="evenement">
+                                <span>EVENEMENTS</span>
+                            </li>
+                            <li>
+                                <img src="../../../image/galerie.png" alt="galerie">
+                                <span>GALERIE</span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="container_nav_system">
+                    <div>
+                        <div>
+                            <h5>Sytème</h5>
+                        </div>
+                        <ul>
+                            <li>
+                                <img src="../../../image/message.png" alt="faq">
+                                <span>FAQ</span>
+                            </li>
+                            <li>
+                                <img src="../../../image/courrier.png" alt="messagerie">
+                                <span>MESSAGERIE</span>
+                            </li>
+                            <li>
+                                <img src="../../../image/compte.png" alt="admin">
+                                <span>ADMIN</span>
+                                
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="container_nav_connexion">
+                <button class="btn-primary">DECONNEXION</button>
+            </div>
+        </nav>
+    </div>
+
+</body>
+</html>
