0d985f28d22b0870c910d4086289ca70679d943a
arakot <nyarovantsoanathan.rakotonoera@eemi.com>
2025-05-23 15:43:43 +0200

    Optimizing the calendar component by Nathan

--- pages/admin/events.php
+++ pages/admin/events.php
@@ -11,10 +11,65 @@
 <body>
     <div class="main-container">
         <h1>ÉVÈNEMENTS</h1>
-        <div class="responsive calendar-container">
+        <div class="calendar-container">
             <div class="calendar">
-                
-            </div>
+                <div class="header-calendar">
+                    <button>&lt;</button>
+                    <div class="month">JUILLET<br>2025</div>
+                    <button>&gt;</button>
+                </div>
+                <div class="day-names">
+                    <div>LUNDI</div>
+                    <div>MARDI</div>
+                    <div>MERCREDI</div>
+                    <div>JEUDI</div>
+                    <div>VENDREDI</div>
+                    <div>SAMEDI</div>
+                    <div>DIMANCHE</div>
+                </div>
+                <div class="grid-calendar">
+                    <div class="day adjacent">30</div>
+                    <div class="day">1</div>
+                    <div class="day">2</div>
+                    <div class="day">3</div>
+                    <div class="day">4</div>
+                    <div class="day">5</div>
+                    <div class="day">6</div>
+
+                    <div class="day">7</div>
+                    <div class="day">8</div>
+                    <div class="day">9</div>
+                    <div class="day">10</div>
+                    <div class="day">11</div>
+                    <div class="day">12</div>
+                    <div class="day">13</div>
+
+                    <div class="day">14</div>
+                    <div class="day">15</div>
+                    <div class="day">16</div>
+                    <div class="day">17</div>
+                    <div class="day">18</div>
+                    <div class="day">19</div>
+                    <div class="day">20</div>
+
+                    <div class="day">21</div>
+                    <div class="day">22</div>
+                    <div class="day">23</div>
+                    <div class="day">24</div>
+                    <div class="day">25</div>
+                    <div class="day">26</div>
+                    <div class="day">27</div>
+
+                    <div class="day">28</div>
+                    <div class="day">29</div>
+                    <div class="day">30</div>
+                    <div class="day">31</div>
+                    <div class="day adjacent">1</div>
+                    <div class="day adjacent">2</div>
+                    <div class="day adjacent">3</div>
+                </div>
+                </div>
+
         </div>
 
         <div class="responsive event-table">
