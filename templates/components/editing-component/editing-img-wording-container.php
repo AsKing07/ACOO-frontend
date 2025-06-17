7b2bd5594e7ce1f152c9174d0f1894755d5143a5
arakot <nyarovantsoanathan.rakotonoera@eemi.com>
2025-06-17 11:04:53 +0200

    Creating component editing-img-wording-container, creating and stylizing the palmares dashboard by Nathan

---
+++ templates/components/editing-component/editing-img-wording-container.php
@@ -0,0 +1,29 @@
+      <div class="editing-img-wording-container">
+         <div class="edit-img">
+            <img src="">
+         </div>
+
+         <div class="edit-description">
+            <h2>Paul Bernard</h2>
+            <h3>Champion de France 2022</h3>
+
+            <div class="desc-wording">
+               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                  Excepteur sint occaecat cupidatat non proident, 
+                  sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+
+            <div class="edit-btnS">
+              <a href="#" class="">
+                <button class="btn-primary">Modifier</button>
+              </a>
+
+              <a href="#" class="">
+                <button class="btn-primary">Supprimer</button>
+              </a>
+            </div>
+         </div>
+      </div>
