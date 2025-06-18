0f5e63d1f677f7a83e64da4b72302b75216a85e6
Charbel Sonon <104888004+AsKing07@users.noreply.github.com>
2025-06-18 23:21:21 +0100

    Update config.js

--- service/config.js
+++ service/config.js
@@ -2,8 +2,8 @@
 
 export const API_BASE_URL="https://acoobackend.charbelsnn.com";
 
-export const ANALYTICS_API_URL = `http://localhost:3001/api/plausible`;
-// export const ANALYTICS_API_URL = `https://acoo-plausible-analytics-back.onrender.com/api/plausible`;
+//export const ANALYTICS_API_URL = `http://localhost:3001/api/plausible`;
+export const ANALYTICS_API_URL = `https://acoo-plausible-analytics-back.onrender.com/api/plausible`;
 
 
 
